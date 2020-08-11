8d288c6a09ae89f425a01b97e64fdf9cd1ec256e
evyatar54 <evyatar.sivan@gmail.com>
2020-08-11 18:43:41 +0300

    Update measurments.py

--- ci/measurments.py
+++ ci/measurments.py
@@ -18,14 +18,11 @@ teams = {
         "assignee": "asiana-nabary"
     },
     "team2": {
-        "assignee": "AsiaNabary"
-#                "owner": "nabaryasia2"
+        "assignee": ["AsiaNabary" , "nabaryasia2"]
 
     },
     "team3": {
-        "assignee": "hurarabin2020" 
-#             "owner": "yonesheba200",
-#                        "owner": "nooralquranhura"
+        "assignee": ["hurarabin2020" , "yonesheba200", "nooralquranhura"]
 
 
     },
@@ -36,16 +33,25 @@ teams = {
 
 for team in teams:
     print("Closed issues for team: %s" % team)
-    url = api_endpoint + "/issues?assignee=%s&state=closed" % teams[team]["assignee"]
-    resp = get(url, auth=(auth_user, auth_pass), headers=headers).json()
-    print("    %s" % len(resp))
+    sum=0
+    for assignee in team["assignee"]:
+        url = api_endpoint + "/issues?assignee=%s&state=closed" % teams[team]["assignee"]
+        resp = get(url, auth=(auth_user, auth_pass), headers=headers).json()
+        sum+=len(resp)
+    print("    %s" % sum)
 
     print("Open bugs for team: %s" % team)
-    url = api_endpoint + "/issues?assignee=%s&state=open&labels=bug" % teams[team]["assignee"]
-    resp = get(url, auth=(auth_user, auth_pass), headers=headers).json()
-    print("    %s" % len(resp))
+    sum=0
+    for assignee in team["assignee"]:
+        url = api_endpoint + "/issues?assignee=%s&state=open&labels=bug" % teams[team]["assignee"]
+        resp = get(url, auth=(auth_user, auth_pass), headers=headers).json()
+        sum+=len(resp)
+    print("    %s" % sum)
 
     print("Failed deployments for team: %s" % team)
-    url = api_endpoint + "/actions/runs?conclusion=failiure&assignee=%s" % teams[team]["assignee"]
-    resp = get(url, headers=headers).json()
-    print("    %s" % resp["total_count"])
+    for assignee in team["assignee"]:
+        sum=0
+        url = api_endpoint + "/actions/runs?conclusion=failiure&assignee=%s" % teams[team]["assignee"]
+        resp = get(url, headers=headers).json()
+        sum+=resp["total_count"]
+    print("    %s" % sum)
